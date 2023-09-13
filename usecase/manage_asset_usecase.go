91237dd58a67db31ce1d088eb78f1a8b94d5f2e3
Arga Satya Mulyono <argasatya16@gmail.com>
2023-09-13 20:53:11 +0700

    [FIXING] fix bug at manage asset usecase

--- usecase/manage_asset_usecase.go
+++ usecase/manage_asset_usecase.go
@@ -24,24 +24,26 @@ func (m *manageAssetUsecase) CreateTransaction(payload dto.ManageAssetRequest) e
 	if payload.NikStaff == "" {
 		return fmt.Errorf("nik staff cannot empty")
 	}
-
-	for _, v := range payload.ManageAssetDetailReq {
-		if v.IdAsset == "" {
+	
+	var newManageDetail []dto.ManageAssetDetailRequest
+	for _, detail := range payload.ManageAssetDetailReq {
+		if detail.IdAsset == "" {
 			return fmt.Errorf("id asset cannot empty")
 		}
 
-		if v.Status == "" {
+		if detail.Status == "" {
 			return fmt.Errorf("status cannot empty")
 		}
 
-		if v.TotalItem < 0 {
+		if detail.TotalItem < 0 {
 			return fmt.Errorf("total item must equal than 0")
 		}
 
-		_, err := m.assetUC.FindById(v.IdAsset)
+		_, err := m.assetUC.FindById(detail.IdAsset)
 		if err != nil {
 			return fmt.Errorf(err.Error())
 		}
+		newManageDetail = append(newManageDetail, detail)
 	}
 
 	_, err := m.staffUC.FindById(payload.NikStaff)
@@ -49,6 +51,7 @@ func (m *manageAssetUsecase) CreateTransaction(payload dto.ManageAssetRequest) e
 		return fmt.Errorf(err.Error())
 	}
 
+	payload.ManageAssetDetailReq = newManageDetail
 	payload.SubmisstionDate = time.Now()
 	payload.ReturnDate = payload.SubmisstionDate.AddDate(0, 0, payload.Duration)
 	err = m.repo.CreateTransaction(payload)
@@ -67,5 +70,6 @@ func NewManageAssetUsecase(repo repository.ManageAssetRepository, staffUC StaffU
 	return &manageAssetUsecase{
 		repo:    repo,
 		staffUC: staffUC,
+		assetUC: assetUC,
 	}
 }
