db117303062901030b3f8d7d1bd6fe9e51743f55
Stephaniee <62786809+StephanieAgatha@users.noreply.github.com>
2023-09-18 08:39:04 +0700

    unit testing controller (unfinished)

---
+++ __mock__/usecasemock/manage_assets_usecase_mock.go
@@ -0,0 +1,31 @@
+package usecasemock
+
+import (
+	"final-project-enigma-clean/model"
+	"final-project-enigma-clean/model/dto"
+	"github.com/stretchr/testify/mock"
+)
+
+type ManageAssetsMock struct {
+	mock.Mock
+}
+
+func (m ManageAssetsMock) CreateTransaction(payload dto.ManageAssetRequest) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m ManageAssetsMock) ShowAllAsset() ([]model.ManageAsset, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m ManageAssetsMock) FindByTransactionID(id string) ([]model.ManageDetailAsset, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m ManageAssetsMock) DownloadAssets() ([]byte, error) {
+	//TODO implement me
+	panic("implement me")
+}
